2ac05126a00fc7af8cfb036589623a7c2272a1b9
gungla <gglahn@gmail.com>
2021-11-02 15:58:49 -0300

    web dinámico con Angular, Firebase

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescricpion } from '../../interfaces/producto-descripcion.interface';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  producto: ProductoDescricpion;
+  id: string;
+
+  constructor( private route: ActivatedRoute,
+               public productoService: ProductosService ) { }
+
+  ngOnInit() {
+
+    this.route.params
+      .subscribe( parametros => {
+        //console.log(parametros['id']);
+        this.productoService.getProducto(parametros['id'])
+          .subscribe( (producto: ProductoDescricpion) => {
+            this.id = parametros['id'];
+            this.producto = producto;
+            //console.log(producto);
+          });
+      });
+
+  }
+
+}
